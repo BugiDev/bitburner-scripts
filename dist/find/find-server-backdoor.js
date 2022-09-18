940aedeeac3bf96c6bec5ac6649e15c750a6975a
Bogdan Begovic <bogdanbegovic@gmail.com>
2022-09-19 01:24:36 +0200

    Improved backdoor script

--- dist/find/find-server-backdoor.js
+++ dist/find/find-server-backdoor.js
@@ -1,11 +1,15 @@
 import { findServer } from '/find/find-server';
 import { log } from '/util/log';
+import { validateServerName } from '/util/validation';
+export function autocomplete(data) {
+    return [...data.servers]; // This script autocompletes the list of servers.
+}
 export async function main(ns) {
     const serverName = ns.args[0];
     const debug = (ns.args[1] || true);
+    validateServerName(serverName);
     const serverPaths = findServer(ns, 'home', 'home');
     const pathWithBackdoor = `${serverPaths[serverName]} backdoor;`;
-    log(ns, pathWithBackdoor, debug);
     log(ns, 'Copied path to clipboard!', debug);
     await navigator.clipboard.writeText(pathWithBackdoor);
 }
--- src/find/find-server-backdoor.ts
+++ src/find/find-server-backdoor.ts
@@ -1,13 +1,19 @@
-import { NS } from '@ns';
+import { AutocompleteData, NS } from '@ns';
 import { findServer } from '/find/find-server';
 import { log } from '/util/log';
+import { validateServerName } from '/util/validation';
+
+export function autocomplete(data: AutocompleteData) {
+  return [...data.servers]; // This script autocompletes the list of servers.
+}
 
 export async function main(ns: NS): Promise<void> {
   const serverName = ns.args[0] as string;
   const debug = (ns.args[1] || true) as boolean;
+  validateServerName(serverName);
+
   const serverPaths = findServer(ns, 'home', 'home');
   const pathWithBackdoor = `${serverPaths[serverName]} backdoor;`;
-  log(ns, pathWithBackdoor, debug);
   log(ns, 'Copied path to clipboard!', debug);
   await navigator.clipboard.writeText(pathWithBackdoor);
 }
